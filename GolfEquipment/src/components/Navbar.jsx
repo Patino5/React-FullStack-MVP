c1c09aeac6d38783be0ea4ee0f275ac9d306d910
Patino5 <106916823+Patino5@users.noreply.github.com>
2023-12-17 04:27:30 -0600

    added scroll to my clubs section on button click

--- GolfEquipment/src/components/Navbar.jsx
+++ GolfEquipment/src/components/Navbar.jsx
@@ -1,12 +1,19 @@
+import AddButton from "./AddButton"
 import Button from "./AddButton"
 
 const Navbar = () => {
+
+    const scrollToClubs = () => {
+        const clubSection = document.querySelector('#clubsSection')
+        clubSection.scrollIntoView({behavior: 'smooth'})
+    }
+
     return (
         <div className="navbar">
             <h1>My Club Caddie</h1>
             <span className="navItems">
-                <button>My Clubs</button>
-                <Button />
+                <button onClick={scrollToClubs}>My Clubs</button>
+                <AddButton />
             </span>
         </div>
     )
