7955ed3faf4cd86cbb561e2a564e499f3128411f
Patino5 <106916823+Patino5@users.noreply.github.com>
2023-12-19 17:18:00 -0600

    updated

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static('dist'))
 
 // Get all Clubs 
 app.get('/api/clubs', async (req, res) => {
